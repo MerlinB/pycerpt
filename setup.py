ac55a19087f2565a933e3fff683720276f04a33d
Merlin Buczek <merlin.buczek@gmail.com>
2019-01-11 21:54:03 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycerpt',
-    version='0.2.0',
+    version='0.2.1',
     author='Merlin Buczek',
     author_email='merlin.buczek@gmail.com',
     description='A cli utility for generating excerpts from highlighted PDFs',
