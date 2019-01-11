1e845d7c5beef7f75187e5abf6dec7c4c4979deb
Merlin Buczek <merlin.buczek@gmail.com>
2019-01-11 14:27:15 +0100

    Test exceptions

--- tests/test_excerpt.py
+++ tests/test_excerpt.py
@@ -1,4 +1,19 @@
-class TestClass:
-    def test_one(self):
-        x = "this"
-        assert 'h' in x
+import pytest
+
+from pycerpt.excerpt import Excerpt
+
+
+class TestExceptions:
+    @pytest.fixture
+    def excerpt(self):
+        return Excerpt(
+            title='testtitle',
+            paragraphs=['testparagraph1', 'testparagraph2'])
+
+    def test_pdf_exception(self, excerpt, tmpdir):
+        with pytest.raises(ImportError):
+            excerpt.save(f'{tmpdir}/test.pdf')
+
+    def test_unknown_format(self, excerpt, tmpdir):
+        with pytest.raises(NotImplementedError):
+            excerpt.save(f'{tmpdir}/test.unknown')
